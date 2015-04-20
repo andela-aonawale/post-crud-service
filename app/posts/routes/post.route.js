165f687381e69ce1bc29b501c0d570e0debcbc37
ahmed onawale <ahmed.onawale@andela.co>
2015-04-21 00:07:21 +0100

    add author routes

---
+++ app/posts/routes/post.route.js
@@ -0,0 +1,13 @@
+var posts = require('../controllers/post.controller');
+
+module.exports = function(router){
+	router.route('/posts').get(posts.getAllPost);
+	router.route('/posts/:id').get(posts.getOnePost);
+	router.route('/posts').post(posts.createPost);
+	router.route('/posts/:id').put(posts.updatePost);
+	router.route('/posts/:id').delete(posts.deletePost);
+
+	router.route('/posts/author/:author')
+	.delete(posts.deleteUserPosts)
+	.put(posts.updatePostAuthor);
+};
