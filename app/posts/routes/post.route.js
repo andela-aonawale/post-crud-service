50cff3d9ebe1f93047486ac70242e8d401284f94
ahmed onawale <ahmed.onawale@andela.co>
2015-04-21 17:03:08 +0100

    chain routes

--- app/posts/routes/post.route.js
+++ app/posts/routes/post.route.js
@@ -1,11 +1,14 @@
 var posts = require('../controllers/post.controller');
 
 module.exports = function(router){
-	router.route('/posts').get(posts.getAllPost);
-	router.route('/posts/:id').get(posts.getOnePost);
-	router.route('/posts').post(posts.createPost);
-	router.route('/posts/:id').put(posts.updatePost);
-	router.route('/posts/:id').delete(posts.deletePost);
+	router.route('/posts')
+	.get(posts.getAllPost)
+	.post(posts.createPost);
+
+	router.route('/post/:id')
+	.get(posts.getOnePost)
+	.put(posts.updatePost)
+	.delete(posts.deletePost);
 
 	router.route('/posts/author/:author')
 	.delete(posts.deleteUserPosts)
